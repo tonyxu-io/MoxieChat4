9fdc0388121053ea77735cab6a8ef5e6d23b6942
Tony Xu <yihan.xu@gmail.com>
2018-03-23 21:21:25 -0700

    Initialize project

---
+++ MoxieChat/AppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.h
+//  MoxieChat
+//
+//  Created by Moxtra on 2017/6/26.
+//  Copyright © 2017年 Moxtra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
+
---
+++ MoxieChat/ViewController.h
@@ -0,0 +1,17 @@
+//
+//  ViewController.h
+//  MoxieChat
+//
+//  Created by Moxtra on 2017/6/26.
+//  Copyright © 2017年 Moxtra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+{
+    NSString *uniqueID;
+}
+@property(nonatomic, copy) NSString* uniqueID;
+@end
+
